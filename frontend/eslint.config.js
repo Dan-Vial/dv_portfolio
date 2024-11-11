967e754db6a995cd5b19ce656638436ecd6fda35
Dan VIAL <dan.vial@dbmail.com>
2024-11-11 18:53:29 +0100

    FrontEnd => edit config: vite, ts, eslint, package

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,21 +1,30 @@
-import js from '@eslint/js'
+import pluginJs from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [pluginJs.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
+    files: ['src/**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    settings: { react: { version: '18.3' } },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,22 +32,24 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'indent': [
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@typescript-eslint/no-unused-vars': [
         'error',
-        2
-      ],
-      'linebreak-style': [
-        'error',
-        'unix'
-      ],
-      'quotes': [
-        'error',
-        'single'
-      ],
-      'semi': [
-        'error',
-        'never'
+        {
+          'args': 'all',
+          'argsIgnorePattern': '^_',
+          'caughtErrors': 'all',
+          'caughtErrorsIgnorePattern': '^_',
+          'destructuredArrayIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'ignoreRestSiblings': true
+        }
       ]
-    },
-  },
+    }
+  }
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,48 +1,50 @@
-import { AliasOptions, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import fs from 'fs'
-import { resolve } from 'path'
-import tsconfig from './tsconfig.app.json'
-
-function pathTsToVite(tsconfig: { compilerOptions: { paths: { [key: string]: string[] } } }): AliasOptions | undefined {
-  const alias: AliasOptions | undefined = {}
-  const paths = tsconfig.compilerOptions.paths
-
-  for (const element in paths) {
-    const nameKey: string = element.replace('/*', '')
-    const path: string = resolve(__dirname, paths[element][0].replace('/*', ''))
-
-    alias[nameKey] = path
-
-    // TypeScript use Array / viteJs use string
-    // for (const key in paths[element]) {
-    //   alias[nameKey].push(paths[element][key])
-    // }
-  }
-  return alias
-}
+// import { resolve } from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    strictPort: true,
-    port: 9443,
-    https: {
-      key: fs.readFileSync('../../../ssl/certs/localhost.key'),
-      cert: fs.readFileSync('../../../ssl/certs/localhost.crt'),
+export default defineConfig(({ mode }) => {
+  const isSSR = mode === 'ssr'
+
+  return {
+    plugins: [
+      tsconfigPaths({
+        configNames: ['tsconfig.json']
+      }),
+      react()
+    ],
+    server: {
+      port: 4000,
+      proxy: {
+        '/api': 'http://localhost:4001',
+      }
+    },
+    build: {
+      sourcemap: true,
+      ssrManifest: !isSSR,
+      ssr: isSSR ? 'src/mainSSR.tsx' : false,
+      outDir: 'dist',
+      emptyOutDir: !isSSR,
+      rollupOptions: {
+        input: (() => {
+          if (isSSR) {
+            return 'src/mainSSR.tsx'
+          } else {
+            return 'index.html'
+          }
+        })(),
+        output: {
+          entryFileNames: isSSR ? 'assets/mainSSR-[hash].js' : 'assets/[name]-[hash].js',
+        },
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          api: 'modern-compiler'
+        }
+      }
     }
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        data: resolve(__dirname, 'src/data/Data.tsx'),
-        // mainssr: resolve(__dirname, 'src/mainSSR.tsx'),
-      },
-    }
-  },
-  resolve: {
-    alias: pathTsToVite(tsconfig)
-  },
+  }
 })
