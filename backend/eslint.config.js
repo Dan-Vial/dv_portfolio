6ac569a5eb03ef86c9856425920b36f425224b7c
Dan VIAL <dan.vial@dbmail.com>
2024-11-11 16:46:31 +0100

    BackEnd => edit config: eslint, rollup, tsconfig, package.

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,30 +1,33 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,tsx}'] },
+  { files: ['src/**/*.{ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     rules: {
-      'indent': [
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@typescript-eslint/no-unused-vars': [
         'error',
-        2
-      ],
-      'linebreak-style': [
-        'error',
-        'unix'
-      ],
-      'quotes': [
-        'error',
-        'single'
-      ],
-      'semi': [
-        'error',
-        'never'
+        {
+          'args': 'all',
+          'argsIgnorePattern': '^_',
+          'caughtErrors': 'all',
+          'caughtErrorsIgnorePattern': '^_',
+          'destructuredArrayIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'ignoreRestSiblings': true
+        }
       ]
     }
   }
